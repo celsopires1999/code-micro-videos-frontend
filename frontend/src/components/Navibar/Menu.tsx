93974b4743b0be98c4010e532f0153fb336e5d8c
celsopires1999 <celso.pires1999@gmail.com>
2021-11-02 22:20:53 -0300

    montando breadcrumb

--- frontend/src/components/Navibar/Menu.tsx
+++ frontend/src/components/Navibar/Menu.tsx
@@ -2,6 +2,14 @@
 import { IconButton, Menu as MuiMenu, MenuItem } from '@material-ui/core';
 import * as React from 'react';
 import MenuIcon from '@material-ui/icons/Menu';
+import routes, { MyRouteProps } from '../../routes';
+import { Link } from 'react-router-dom';
+
+const listRoutes = [
+    'dashboard',
+    'categories.list'
+];
+const menuRoutes = routes.filter(route => listRoutes.includes(route.name));
 
 export const Menu = () => {
     const [anchorEl, setAnchorEl] = React.useState(null);
@@ -32,9 +40,18 @@ export const Menu = () => {
             transformOrigin={ {vertical: 'top', horizontal:'center'} }
             getContentAnchorEl={ null }
         >
-            <MenuItem onClick={ handleClose }>
-                Categorias
-            </MenuItem>
+            {
+                listRoutes.map(
+                    (routeName, key) => {
+                        const route = menuRoutes.find(route => route.name === routeName) as MyRouteProps;
+                        return (
+                            <MenuItem key={ key } component={ Link } to={ route.path as string} onClick={ handleClose }>
+                                { route.label }
+                            </MenuItem>
+                        )
+                    }
+                )
+            }
         </MuiMenu>
         </React.Fragment>
     );
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,22 +1,46 @@
 import { RouteProps } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
-import Category from "../pages/category/List"
+import CategoryList from "../pages/category/List"
 
-interface MyRouteProps extends RouteProps {
+export interface MyRouteProps extends RouteProps {
+    name: string;
     label: string;
 }
 
 const routes: MyRouteProps[] = [
     {
+        name: 'dashboard',
         label: 'Dashboard',
         path: '/',
         component: Dashboard,
         exact: true
     },
     {
+        name: 'categories.list',
         label: 'Listar categorias',
         path: '/categories',
-        component: Category,
+        component: CategoryList,
+        exact: true
+    },
+    {
+        name: 'categories.create',
+        label: 'Criar categoria',
+        path: '/categories/create',
+        component: CategoryList,
+        exact: true
+    },
+    {
+        name: 'categories.edit',
+        label: 'Editar categoria',
+        path: '/categories/:id/edit',
+        component: CategoryList,
+        exact: true
+    },
+    {
+        name: 'categories.edit',
+        label: 'Testando',
+        path: '/categories/:id/edit/:name',
+        component: CategoryList,
         exact: true
     },
 ];
