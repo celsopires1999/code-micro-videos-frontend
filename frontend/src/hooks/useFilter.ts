32d79dbc28b426897e6087d009599a829b2421ce
celsopires1999 <celso.pires1999@gmail.com>
2022-01-09 23:07:07 -0300

    Criando push state com history parte 1

--- frontend/src/hooks/useFilter.ts
+++ frontend/src/hooks/useFilter.ts
@@ -3,17 +3,23 @@ import { Dispatch, Reducer, useReducer, useState } from "react";
 import reducer, { Creators, INITIAL_STATE } from "../store/filter";
 import { Actions as FilterActions, State as FilterState } from '../store/filter/types'
 import { useDebounce } from 'use-debounce';
+import { useHistory } from 'react-router';
+import { History } from 'history';
 
-interface FiltroManagerOptions {
+interface FilterManagerOptions {
     columns: MUIDataTableColumn[];
     rowsPerPage: number;
     rowsPerPageOptions: number[];
     debouncedTime: number;
+    history: History;
 }
 
-export default function useFilter(options: FiltroManagerOptions) {
+interface UseFilterOptions extends Omit<FilterManagerOptions, 'history'> { }
 
-    const filterManager = new FilterManager(options);
+export default function useFilter(options: UseFilterOptions) {
+
+    const history = useHistory();
+    const filterManager = new FilterManager({ ...options, history });
     //TODO: pegar o state da url
     const [filterState, dispatch] = useReducer<Reducer<FilterState, FilterActions>>(reducer, INITIAL_STATE);
     const [debouncedFilterState] = useDebounce(filterState, options.debouncedTime);
@@ -40,12 +46,14 @@ export class FilterManager {
     columns: MUIDataTableColumn[];
     rowsPerPage: number;
     rowsPerPageOptions: number[];
+    history: History;
 
-    constructor(options: FiltroManagerOptions) {
-        const { columns, rowsPerPage, rowsPerPageOptions } = options;
+    constructor(options: FilterManagerOptions) {
+        const { columns, rowsPerPage, rowsPerPageOptions, history } = options;
         this.columns = columns;
         this.rowsPerPage = rowsPerPage;
         this.rowsPerPageOptions = rowsPerPageOptions;
+        this.history = history;
     }
 
     changeSearch(value) {
@@ -88,5 +96,16 @@ export class FilterManager {
             newText = text.value
         }
         return newText;
+    }
+
+    pushHistory() {
+        const newLocation = {
+            pathname: 'endereço',
+            search: '?search=teste&page=1&sort=name&dir=asc',
+            state: {
+
+            }
+        }
+        this.history.push(newLocation);
     }
 }
