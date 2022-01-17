9203326ed0b992b9446cb3f81bee10baba3f5abb
celsopires1999 <celso.pires1999@gmail.com>
2022-01-17 00:24:21 -0300

    Relacionamento de Cast Member com Video

--- backend/.phpstorm.meta.php
+++ backend/.phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2022-01-16 01:24:25.
+    * Generated on 2022-01-16 22:29:08.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
